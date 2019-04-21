0d308d47c17da635d9645d7dc461d912e08e640c
ghspbravo <gmspbravo@gmail.com>
2019-04-21 16:37:53 +0500

    FIX: bug with fixed topbanner on small height screens

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -7,20 +7,26 @@ import mailIcon from './mail.svg'
 
 export default function footer() {
 	return (
-		<div className="container mt-5 mb-2">
-			<div className="mb-1">
-				<p><b>Ищи нас тут:</b></p>
-			</div>
-			<div className="row no-gutters">
-				<a className="mr-1" href="https://www.instagram.com/katadzzze/" target="_blank" rel="noopener noreferrer">
-					{bgIcon(instIcon)}
-				</a>
-				<a className="mr-1" href="https://vk.com/katadzzze" target="_blank" rel="noopener noreferrer">
-					{bgIcon(vkIcon)}
-				</a>
-				<a href="mailto:katadzeguide@mail.ru" target="_blank" rel="noopener noreferrer">
-					{bgIcon(mailIcon)}
-				</a>
+		<div className="pt-5 pb-2" style={{
+			backgroundColor: 'white',
+			position: 'relative',
+			zIndex: 1
+		}}>
+			<div className="container">
+				<div className="mb-1">
+					<p><b>Ищи нас тут:</b></p>
+				</div>
+				<div className="row no-gutters">
+					<a className="mr-1" href="https://www.instagram.com/katadzzze/" target="_blank" rel="noopener noreferrer">
+						{bgIcon(instIcon)}
+					</a>
+					<a className="mr-1" href="https://vk.com/katadzzze" target="_blank" rel="noopener noreferrer">
+						{bgIcon(vkIcon)}
+					</a>
+					<a href="mailto:katadzeguide@mail.ru" target="_blank" rel="noopener noreferrer">
+						{bgIcon(mailIcon)}
+					</a>
+				</div>
 			</div>
 		</div>
 	)
