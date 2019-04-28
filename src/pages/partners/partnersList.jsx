e1168af49c7afca93199a6bff2a4ae06782363c2
ghspbravo <gmspbravo@gmail.com>
2019-04-28 14:31:49 +0500

    RESTYLE: partners feedback

--- src/pages/partners/partnersList.jsx
+++ src/pages/partners/partnersList.jsx
@@ -26,44 +26,42 @@ export default function partnersList(router) {
 	const partnerId = router.match.params.id
 
 	const [modal, openModal] = useModal()
+	const [modalYes, openYesModal] = useModal()
+	const [modalNo, openNoModal] = useModal()
 
 	const activateCoupon = useActions(actions => actions.partners.activateCoupon)
 
-	const question = useRef()
-	const success = useRef()
-	const notSuccess = useRef()
+	const positiveFeedback = useCallback((e) => {
+		openYesModal(<div className="col-lg-6 col-sm-8 col-12 px-0 mx-auto">
+			<i className="partners-feedback-icon fas fa-check-circle"></i>
+			<h5>Ура!</h5>
+			<p>Мы рады создавать возможности для тебя</p>
+		</div>, e)
+	})
+
+	const negativeFeedback = useCallback((e) => {
+		openNoModal(<div className="col-lg-6 col-sm-8 col-12 px-0 mx-auto">
+			<i className="partners-feedback-icon fas fa-times-circle"></i>
+			<h5>Сожалеем :(</h5>
+			<p>Напиши нам в чем причина</p>
+			<Link className="button" to='/contacts'>Написать</Link>
+		</div>, e)
+	})
 
 	const handleActivateCoupon = useCallback(id => {
 		activateCoupon(id).then(isSuccess => {
 			isSuccess &&
 				openModal(<div>
-					<div className="col-lg-6 col-sm-8 col-10 px-0 mx-auto">
+					<div className="col-lg-6 col-sm-8 col-12 px-0 mx-auto">
 						<h4 className="text-center">Активировано</h4>
-						<div ref={question}>
+						<div>
 							<p>Получил ли ты скидку?</p>
 
 							<div className="row no-gutters">
-								<button onClick={() => {
-									question.current.style.display = 'none'
-									success.current.style.display = 'block'
-								}}>Да</button>
+								<button className="partners-feedback-button no-style" onClick={positiveFeedback}><i className="fas fa-thumbs-up"></i></button>
 
-								<button onClick={() => {
-									question.current.style.display = 'none'
-									notSuccess.current.style.display = 'block'
-								}} className="button ml-auto">Нет</button>
+								<button className="partners-feedback-button no-style ml-auto" onClick={negativeFeedback}><i className="fas fa-thumbs-down"></i></button>
 							</div>
-						</div>
-						<div ref={success} style={{
-							display: 'none'
-						}}>
-							<p>Мы рады создавать возможности для тебя</p>
-						</div>
-						<div ref={notSuccess} style={{
-							display: 'none'
-						}}>
-							<p>Напиши нам в чем причина</p>
-							<Link className="button" to='/contacts'>Написать</Link>
 						</div>
 					</div>
 				</div>)
@@ -112,6 +110,8 @@ export default function partnersList(router) {
 				{partners[partnerId] && partners[partnerId].length
 					? <div className="row justify-content-center">
 						{modal}
+						{modalYes}
+						{modalNo}
 						{partners[partnerId] &&
 							partners[partnerId].map((partner, index) => <div key={index} className="col-md-6 col-12 mb-3">
 								{cardPartner(
